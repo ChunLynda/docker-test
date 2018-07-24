e1e4437b82024a79713fa1ba498b7fdfcbd232db
vp-lchun <lynda.chun@vecteurplus.com>
2018-07-24 17:16:28 +0700

    first commit

---
+++ node.js
@@ -0,0 +1,8 @@
+console.log("start");
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!\nThis is testing part!\n');
+}).listen(8080);
