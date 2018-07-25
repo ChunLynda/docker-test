1e2cfcac210264711cd4549125c1314dfb1b377a
ChunLynda <chunlynda1@gmail.com>
2018-07-25 15:28:46 +0700

    Update node.js

--- node.js
+++ node.js
@@ -4,6 +4,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!\nThis is testing part!\n');
-    res.end('Hello World1!\nThis is testing part1!\n');
+    console.log('body');
+    res.end('Hello World!\nThis is testing part!\nThis is the second test.');
 }).listen(8080);
